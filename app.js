a8a65d1d9e7da1230bd6237118c19d837f7a73a3
seclud3d <ez.lvl.faceit@gmail.com>
2021-02-15 23:18:21 +0200

    Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+VANTA.NET({
+    el: "#turbo",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x0447ff,
+    backgroundColor: 0x0,
+    points: 14.00,
+    maxDistance: 16.00,
+    spacing: 16.00
+  })
